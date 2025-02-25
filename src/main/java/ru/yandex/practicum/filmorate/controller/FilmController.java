4797749f3211ba85ad66e018c7821119ab6a5952
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-02-25 16:54:37 +0300

    Добавить модели Film, User; Добавить для валидации данных пакет exception с классами: ErrorHandlingControllerAdvice, ValidationErrorResponse, Violation; Добавить контроллер FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,78 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Validated
 @RestController
+@RequestMapping("api/v1/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    /**
+     * Генерация уникального идентификатора
+     *
+     * @return long
+     */
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    /**
+     * Добавлвение фильма
+     *
+     * @param film Film
+     * @return ResponseEntity<Film>
+     */
+    @PostMapping
+    public ResponseEntity<Film> create(@Valid @RequestBody Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return ResponseEntity.ok(film);
+    }
+
+    /**
+     * Обновление фильма
+     *
+     * @param newFilm Film
+     * @return ResponseEntity<Film>
+     */
+    @PutMapping
+    public ResponseEntity<Film> update(@Valid @RequestBody Film newFilm) {
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+            return ResponseEntity.ok(newFilm);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Получение всех фильмов
+     *
+     * @return Collection
+     */
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,38 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+
+    private Long id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Описание не должно превышать 200 символов")
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private Duration duration;
+
+    @AssertTrue(message = "дата релиза не может быть раньше 28 декабря 1895 года")
+    public boolean isReleaseDateValid() {
+        LocalDate cinemaBirthday = LocalDate.of(1895, 12, 28);
+        return releaseDate.isAfter(cinemaBirthday);
+    }
+
+    @AssertTrue(message = "Продолжительность фильма не может быть меньше нуля")
+    public boolean isDurationValid() {
+        return duration.isPositive();
+    }
+
 }
